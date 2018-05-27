c591fa6522c08758861c522589f9927bf7f4d749
christopher-ell <christopher.michael.ell@gmail.com>
2018-05-27 19:25:35 +1000

    Add files via upload

--- 5. CointTest.py
+++ 5. CointTest.py
@@ -33,28 +33,28 @@ price_data = pd.read_sql("select * from daily_price;", con=con)
 price_data = price_data[price_data.checks == 1]
 
 
-all_coint_res = pd.DataFrame(columns = ['symbol_id1','symbol_id1','test type', 'p-value'])
+all_coint_res = pd.DataFrame(columns = ['symbol_id1','symbol_id2','test type', 'p-value'])
 row = 1
 
 
-for symb1 in unique_symbols:
+for symb1 in unit_roots:
     
-    for t in range(int(symb1), len(unique_symbols)):
+    for t in range(int(symb1), len(unit_roots)):
 
         data1 = price_data[price_data.symbol_id == symb1]
-        data2 = price_data[price_data.symbol_id == unique_symbols[t]]
+        data2 = price_data[price_data.symbol_id == unit_roots[t]]
     
         print("symbol 1: ", symb1)
-        print("symbol 2: ", unique_symbols[t])
+        print("symbol 2: ", unit_roots[t])
     
     
         if len(data1) == len(data2):
             for ttype in ['c', 'ct']:
-                coint_res = ts.coint(data1['adj_close_price'], data2['adj_close_price'], regression = ttype)
+                coint_res = ts.coint(data1['adj_close_price'], data2['adj_close_price'], trend = ttype)
                 
                 ## The null hypothesis is no cointegration so if the p-value is less than 5%
                 ## this means we reject the null of no cointegration.
                 if coint_res[1] < 0.05:
-                    all_coint_res.loc[row] = [symb1, unique_symbols[t], ttype, coint_res[1]]
+                    all_coint_res.loc[row] = [symb1, unit_roots[t], ttype, coint_res[1]]
                     row += 1
     
