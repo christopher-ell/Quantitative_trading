8281add9fde95f55c7fba69ee520faf6a7782b5f
christopher-ell <christopher.michael.ell@gmail.com>
2018-06-14 18:06:01 +1000

    Move writer
    Move writer to outside functions so not saving over

--- B3_Output_to_excel.py
+++ B3_Output_to_excel.py
@@ -11,22 +11,17 @@ import matplotlib.dates as mdates
 import openpyxl
 
 filepath = 'V:\Staff\Chris\Programming\Python\Quant\Quantstart\Outputs\Template.xlsx'
-sheet_name = "test"
 
+writer = pd.ExcelWriter(filepath, engine='xlsxwriter')
 
-
-
-
-
-
+sheet_name='test'
 
 ## Chart 1
 
-def plot_results(df, ts1, ts2, sheet_name, filepath, cell):
+def plot_results(writer, df, ts1, ts2, sheet_name, filepath, cell):
     
     ## Create Pandas Excel writer using Xlswriter as the engine
-    writer = pd.ExcelWriter(filepath, engine='xlsxwriter')
-    back.to_excel(writer, sheet_name=sheet_name, startrow=1, startcol=1)
+    df.to_excel(writer, sheet_name=sheet_name, startrow=1, startcol=1)
     
     ## Access the Xlswriter workbook and worksheets objects from the dataframe.
     workbook = writer.book
@@ -36,10 +31,10 @@ def plot_results(df, ts1, ts2, sheet_name, filepath, cell):
     chart = workbook.add_chart({'type':'line'})
 
     ## Calculate extremes for axes
-    min_x1 = back[ts1].min()
-    max_x1 = back[ts1].max()
-    min_x2 = back[ts2].min()
-    max_x2 = back[ts2].max()    
+    min_x1 = df[ts1].min()
+    max_x1 = df[ts1].max()
+    min_x2 = df[ts2].min()
+    max_x2 = df[ts2].max()    
     min_x = min(min_x1, min_x2)
     max_x = max(max_x1, max_x2)
 
@@ -81,34 +76,61 @@ def plot_results(df, ts1, ts2, sheet_name, filepath, cell):
     ## Insert chart into worksheet
     worksheet.insert_chart(cell, chart)
 
-    writer.save()
-
-plot_results(back, 'adj_close_price4.0', 'adj_close_price26.0', sheet_name, filepath, 'Q2')
+#    writer.save()
 
 
-### Chart 2
-#
-### Create a chart object 
-#chart = workbook.add_chart({'type':'scatter'})
-#
-### Configure the series of the chart from the dataframe data
-#chart.add_series({
-##        'name':'Series1',
-#        'categories': 'test!$E$3:$E502',
-#        'values':'=test!$C$3:$C502'
-#        })
-#
-##chart.add_series({
-##        'name':'Series2',
-##        'categories': '=test!$D$3:$D502',
-##        'values':'=test!$E$3:$E502'
-##        })
-#
-### Configure chart axis
-#chart.set_x_axis({'name':'Series1'})
-#chart.set_y_axis({'name':'Series2'})
-#
-### Insert chart into worksheet
-#worksheet.insert_chart('Q18', chart)
-#
-#writer.save()
+
+
+## Chart 2
+def plot_scatter_ts(writer, df, ts1, ts2, sheet_name, filepath, cell):
+
+    ## Create Pandas Excel writer using Xlswriter as the engine
+    df.to_excel(writer, sheet_name=sheet_name, startrow=1, startcol=1)
+    
+    ## Access the Xlswriter workbook and worksheets objects from the dataframe.
+    workbook = writer.book
+    worksheet = writer.sheets[sheet_name]
+
+
+    ## Create a chart object 
+    chart = workbook.add_chart({'type':'scatter'})
+
+
+    min_x1 = df[ts1].min()
+    max_x1 = df[ts1].max()
+    min_x2 = df[ts2].min()
+    max_x2 = df[ts2].max()    
+
+    ## Configure the series of the chart from the dataframe data
+    chart.add_series({
+            #        'name':'Series1',
+            'categories': 'test!$E$3:$E502',
+            'values':'=test!$C$3:$C502'
+            })
+
+
+    ## Configure chart axis
+    chart.set_x_axis({'name':ts1,
+                          'min':min_x2,
+                          'max':max_x2})
+    chart.set_y_axis({'name':ts2,
+                          'min':min_x1,
+                          'max':max_x1})
+    
+    chart.set_title({'name':'%s and %s Price Scatterplot' % (ts1, ts2)})
+    
+    chart.set_legend({'none':True})
+    chart.set_chartarea({'border':{'none':True}})
+
+    ## Insert chart into worksheet
+    worksheet.insert_chart(cell, chart)
+    
+#    writer.save()
+
+    
+    
+plot_scatter_ts(writer, back, 'adj_close_price4.0', 'adj_close_price26.0', sheet_name, filepath, 'Q18')
+
+plot_results(writer, back, 'adj_close_price4.0', 'adj_close_price26.0', sheet_name, filepath, 'Q2')
+
+writer.save()
